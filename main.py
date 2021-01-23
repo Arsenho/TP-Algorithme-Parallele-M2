59fb427ae42b57e484b42cb224c050c093277a7b
schatten <anvadele@yahoo.com>
2021-01-23 19:28:00 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from sequentiel import product_matrix_vector, product_matrix_matrix, product_vector_matrix
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    vector = [1, 2]
+    matrix_left = [[1, 2], [3, 4]]
+    matrix_right = [[1, 0], [0, 1]]
+    matrix_right2 = [[1, 1], [1, 1]]
+
+    print(product_matrix_vector(matrix_left, vector))
+    print(product_matrix_matrix(matrix_left, matrix_right))
+    print(product_vector_matrix(vector, matrix_right))
+    print(product_matrix_matrix(matrix_left, matrix_right2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ sequentiel.py
@@ -0,0 +1,42 @@
+def sum_vector(first, second):
+    sum_result = 0
+    if len(first) != len(second):
+        print("Hi")
+    else:
+        for i in range(0, len(first)):
+            sum_result += first[i] * second[i]
+        return sum_result
+
+
+def product_matrix_vector(matrix, vector):
+    product_result = []
+    if len(vector) is not len(matrix[0]):
+        print("Veillez vous rassurer que le nombre de ligne de la matrice est egale au nombre de colone du vecteur")
+    else:
+        for line in matrix:
+            product_result.append(
+                sum_vector(line, vector)
+            )
+        return product_result
+
+
+def product_vector_matrix(vector, matrix):
+    product_result = []
+    if len(vector) is not len(matrix[0]):
+        print("Veillez vous rassurer que le nombre de ligne de la matrice est egale au nombre de colone du vecteur")
+    else:
+        for line in matrix:
+            product_result.append(
+                sum_vector(vector, line)
+            )
+        return product_result
+
+
+def product_matrix_matrix(matrix_left, matrix_right):
+    product_result = []
+    if len(matrix_left[0]) is not len(matrix_right[0]):
+        print("Veillez vous rassurer que le nombre de ligne de la matrice est egale au nombre de colone du vecteur")
+    else:
+        for line in matrix_left:
+            product_result.append(product_vector_matrix(line, matrix_right))
+        return product_result
